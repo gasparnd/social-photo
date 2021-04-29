3469a18faed30d0b10191fc5f57daa4541789c85
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-04-29 10:40:03 -0300

    fix styles bugs

--- src/components/Modales/PhotoDetailsModal.jsx
+++ src/components/Modales/PhotoDetailsModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Modal from './Modal.jsx'
 
 import './PhotoDetailsModal.css'
@@ -10,6 +11,15 @@ const PhotoDetailsModal = ({ isOpen, handleClose, data}) => {
 			handleClose={ handleClose } 
 		>
 			<div className="PhotoContainer">
+				<section className="PhotoContainer__info">
+					<p><strong>Description:</strong> {data.alt_description}</p>
+					<Link className="profile-link" to={`/${data.user.username}`}>
+						<div className="pin-user">
+							<p>{data.user.username}</p>
+							<img src={data.user.profile_image.small} width="25" height="25" alt={`${data.user.username}'s profile`} />
+						</div>
+					</Link>
+				</section>
 				<section className="PhotoContainer__photo">
 					<img src={data.urls.regular} alt={data.alt_description} />
 				</section>
